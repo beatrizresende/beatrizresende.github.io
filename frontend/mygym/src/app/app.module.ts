f3672238f9eca416fae99b839e0762b481805677
Aquiles Silva dos Santos <asantos@marlin.net>
2021-07-16 19:14:29 -0300

    :construction: Criando AppRouting

--- frontend/mygym/src/app/app.module.ts
+++ frontend/mygym/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { DefaultLayoutComponent } from './components/default-layout/default-layout.component';
+
 import { MeusDadosComponent } from './components/meus-dados/meus-dados.component';
 import { DadosPessoaisComponent } from './components/meus-dados/dados-pessoais/dados-pessoais.component';
 import { DadosContatoComponent } from './components/meus-dados/dados-contato/dados-contato.component';
 import { DadosPagamentoComponent } from './components/meus-dados/dados-pagamento/dados-pagamento.component';
-import { SideBarComponent } from './components/side-bar/side-bar.component';
-import { DefaultLayoutComponent } from './components/default-layout/default-layout.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +24,8 @@ import { DefaultLayoutComponent } from './components/default-layout/default-layo
     DefaultLayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
