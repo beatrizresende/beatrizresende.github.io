fd3022fba679e38b7b2bdce82ff494a4d1d63416
Aquiles Silva dos Santos <asantos@marlin.net>
2021-07-16 18:48:25 -0300

    :construction: Criando diretórios.

--- frontend/mygym/src/app/app.module.ts
+++ frontend/mygym/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MeusDadosComponent } from './components/meus-dados/meus-dados.component';
+import { DadosPessoaisComponent } from './components/meus-dados/dados-pessoais/dados-pessoais.component';
+import { DadosContatoComponent } from './components/meus-dados/dados-contato/dados-contato.component';
+import { DadosPagamentoComponent } from './components/meus-dados/dados-pagamento/dados-pagamento.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeusDadosComponent,
+    DadosPessoaisComponent,
+    DadosContatoComponent,
+    DadosPagamentoComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/mygym/src/app/components/meus-dados/dados-pagamento/dados-pagamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dados-pagamento',
+  templateUrl: './dados-pagamento.component.html',
+  styleUrls: ['./dados-pagamento.component.css']
+})
+export class DadosPagamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
