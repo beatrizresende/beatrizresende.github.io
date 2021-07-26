64a43b4e7080dc0c63457010c7932e0684c7a3ae
Aquiles Silva dos Santos <asantos@marlin.net>
2021-07-26 06:15:42 -0300

    :construction: ajustes gerais.

--- frontend/mygym/src/app/components/home/home.component.ts
+++ frontend/mygym/src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
+import { IUsuario } from './../../models/IUsuario';
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public user: IUsuario;
+  public userId: string;
 
-  constructor() { }
+  constructor(
+    private router:  Router,
+    private usuarioService:  UsuarioService,
+    private activatedRoute:  ActivatedRoute,
+    ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
+    this.activatedRoute.queryParams.subscribe(params => {
+      // this.checkParams(params);
+    });
   }
 
+  checkParams(params) {
+    if(params && params.id) {
+      this.usuarioService.getUserById(params.id).subscribe(response => this.user = response);
+    }
+    else {
+      this.router.navigate(['/login']);
+    }
+  }
 }
